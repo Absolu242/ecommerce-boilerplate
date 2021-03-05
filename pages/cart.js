9eeb71ca3f250f3b67a6c33f2b9da853ee93e97f
Rahan <wangleibniz01@gmail.com>
2021-03-05 08:45:31 +0800

    cart page done

--- pages/cart.js
+++ pages/cart.js
@@ -1,3 +1,106 @@
+import Head from "next/head"
+import { useContext, useState, useEffect } from "react"
+import { DataContext } from "../store/GlobalState"
+//import CartItem from '../components/CartItem'
+import Link from "next/link"
+import { getData, postData } from "../utils/fetchData"
+import { useRouter } from "next/router"
+
 export default function cart() {
+  const { state, dispatch } = useContext(DataContext)
+  const { cart, auth, orders } = state
+
+  const [total, setTotal] = useState(0)
+
+  const [address, setAddress] = useState("")
+  const [mobile, setMobile] = useState("")
+
+  const [callback, setCallback] = useState(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    const getTotal = () => {
+      const res = cart.reduce((prev, item) => {
+        return prev + item.price * item.quantity
+      }, 0)
+
+      setTotal(res)
+    }
+
+    getTotal()
+  }, [cart])
+
+  useEffect(() => {
+    const cartLocal = JSON.parse(localStorage.getItem("__next__cart01__devat"))
+    if (cartLocal && cartLocal.length > 0) {
+      let newArr = []
+      const updateCart = async () => {
+        for (const item of cartLocal) {
+          const res = await getData(`product/${item._id}`)
+          const { _id, title, images, price, inStock, sold } = res.product
+          if (inStock > 0) {
+            newArr.push({
+              _id,
+              title,
+              images,
+              price,
+              inStock,
+              sold,
+              quantity: item.quantity > inStock ? 1 : item.quantity,
+            })
+          }
+        }
+
+        dispatch({ type: "ADD_CART", payload: newArr })
+      }
+
+      updateCart()
+    }
+  }, [callback])
+
+  const handlePayment = async () => {
+    if (!address || !mobile)
+      return dispatch({
+        type: "NOTIFY",
+        payload: { error: "Please add your address and mobile." },
+      })
+
+    let newCart = []
+    for (const item of cart) {
+      const res = await getData(`product/${item._id}`)
+      if (res.product.inStock - item.quantity >= 0) {
+        newCart.push(item)
+      }
+    }
+
+    if (newCart.length < cart.length) {
+      setCallback(!callback)
+      return dispatch({
+        type: "NOTIFY",
+        payload: {
+          error: "The product is out of stock or the quantity is insufficient.",
+        },
+      })
+    }
+
+    dispatch({ type: "NOTIFY", payload: { loading: true } })
+
+    postData("order", { address, mobile, cart, total }, auth.token).then(
+      (res) => {
+        if (res.err)
+          return dispatch({ type: "NOTIFY", payload: { error: res.err } })
+
+        dispatch({ type: "ADD_CART", payload: [] })
+
+        const newOrder = {
+          ...res.newOrder,
+          user: auth.user,
+        }
+        dispatch({ type: "ADD_ORDERS", payload: [...orders, newOrder] })
+        dispatch({ type: "NOTIFY", payload: { success: res.msg } })
+        return router.push(`/order/${res.newOrder._id}`)
+      }
+    )
+  }
   return <div>cart</div>
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,49 @@
-import Head from "next/head"
-import styles from "../styles/Home.module.css"
+import { useState, useContext, useEffect } from "react"
+import { DataContext } from "../store/GlobalState"
 
-export default function Home() {
+import { getData } from "../utils/fetchData"
+import filterSearch from "../utils/filterSearch"
+import { useRouter } from "next/router"
+import Filter from "../components/Filter"
+
+export default function Home(props) {
+  const [products, setProducts] = useState(props.products)
+
+  const [page, setPage] = useState(1)
+  const router = useRouter()
+
+  useEffect(() => {
+    setProducts(props.products)
+  }, [props.products])
+
+  useEffect(() => {
+    if (Object.keys(router.query).length === 0) setPage(1)
+  }, [router.query])
+
+  const handleLoadmore = () => {
+    setPage(page + 1)
+    filterSearch({ router, page: page + 1 })
+  }
+
   return <div>Home</div>
+}
+
+export async function getServerSideProps({ query }) {
+  const page = query.page || 1
+  const category = query.category || "all"
+  const sort = query.sort || ""
+  const search = query.search || "all"
+
+  const res = await getData(
+    `product?limit=${
+      page * 6
+    }&category=${category}&sort=${sort}&title=${search}`
+  )
+  // server side rendering
+  return {
+    props: {
+      products: res.products,
+      result: res.result,
+    }, // will be passed to the page component as props
+  }
 }
